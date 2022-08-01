411d1ddff32b0c798835dafaa6450cb6a7acd1f4
Angelica-zb <angelika.zaborovskaya@gmail.com>
2022-08-01 03:25:53 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+module.exports = function towelSort(matrix) {
+    let str = []
+    let st = []
+    if (matrix == [] || (matrix == undefined)) { return []; } else {
 
-// You should implement your task here.
+        for (let k = 0; k < matrix.length; k++) {
 
-module.exports = function towelSort (matrix) {
-  return [];
+            if (k == 0 || k % 2 == 0) {
+                st = matrix[k].sort((a, b) => a - b)
+                str = str.concat(st);
+            } else {
+                st = matrix[k].sort((a, b) => b - a)
+                str = str.concat(st);
+            }
+        }
+    }
+    return str
 }
